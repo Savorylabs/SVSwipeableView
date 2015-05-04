aa320543169f6023c16ab446cfdc861c96406743
PJ Dillon <knightcode@knightcode.nyc1.savory.com>
2015-05-04 13:03:42 -0400

    Working test build in swift

---
+++ Example/SVSwipeView/SVViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SVViewController.swift
+//  SVSwipeView
+//
+//  Created by PJ Dillon on 5/2/15.
+//  Copyright (c) 2015 PJ Dillon. All rights reserved.
+//
+
+import UIKit
+
+class SVViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+}
